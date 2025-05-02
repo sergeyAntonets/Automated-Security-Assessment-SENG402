208284f82157805b6e0f8a9dc5061931619bb745
sergeyAntonets <san177@uclive.ac.nz>
2025-05-02 16:13:33 +1200

    implement running llama

---
+++ evaluation/runningLLAMA.py
@@ -0,0 +1,24 @@
+from transformers import AutoTokenizer, AutoModelForCausalLM
+import torch
+from huggingface_hub import login
+
+login("TOKEN")
+
+
+model_id = "meta-llama/Llama-3.1-8B"  # Accept the license on Hugging Face!
+
+
+# Load tokenizer and model
+tokenizer = AutoTokenizer.from_pretrained(model_id, use_auth_token=True)
+model = AutoModelForCausalLM.from_pretrained(model_id, use_auth_token=True)
+
+
+def llama_local_generate(prompt, max_tokens=256, temperature=0.7, top_p=0.9):
+    input_ids = tokenizer(prompt, return_tensors="pt").input_ids.to(model.device)
+    output = model.generate(
+        input_ids=input_ids,
+        max_new_tokens=max_tokens,
+        temperature=temperature,
+        top_p=top_p
+    )
+    return tokenizer.decode(output[0], skip_special_tokens=True)
